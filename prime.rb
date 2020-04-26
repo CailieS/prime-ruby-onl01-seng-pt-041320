fea5012ff88342353751ab18a8434b1a27a10e8b
Cailie Skelton <cskelton707@gmail.com>
2020-04-26 00:38:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number <= 1 || number == 0 || number == 1 
     return false
   elsif (2..number - 1).each do |i|
-      if value % i == 0 
+      if number % i == 0 
     return false
     end
     end
